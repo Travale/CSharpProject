52491e22f903d5d54b3002e8b97c6b4707fe646e
Travis Wilkins <uchiwasteel@gmail.com>
2016-10-17 11:52:55 -0600

    powerUps

---
+++ PowerUp.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+public class PowerUps {
+    int power = 10;
+    int duration = 5;
+    string name = "PowerUp";
+
+    public void RunPowerUP (){
+        Console.WriteLine("using PowerUp");
+    }
+
+}
