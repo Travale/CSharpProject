f92d9b7e3defec7c883fe1b5143f80914a0df8a0
Travis <supafatcats@hotmail.com>
2016-10-17 19:50:23 -0600

    commenting out

--- varandfun.cs
+++ varandfun.cs
@@ -17,8 +17,8 @@ public class NewVariables {
     }
 }
 
- // variables are like corrals.  You need a different type of corral for each type of animal or in the case of programming informtion.
+ /* variables are like corrals.  You need a different type of corral for each type of animal or in the case of programming informtion.
 The int variable is used to hold an integer.  If you want to do something with the integer you
 put it in a function.  You can add an integer together with another integer inside the function.
 Create a function with parameters in brackets () to guide it. Call a function inside of another function
-so you can run a new variable through the first function.
+so you can run a new variable through the first function. */
