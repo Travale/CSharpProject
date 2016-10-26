80825164333b9a71bdcb7eb268370d3fe891e526
Travis Wilkins <uchiwasteel@gmail.com>
2016-10-26 11:52:47 -0600

    making game

---
+++ Game.cs
@@ -0,0 +1,38 @@
+using System;
+public class Game {
+
+    public Game () {
+        Health.power = 100;
+        Health.message = "You are getting stronger.";
+        Ammo.message = "You have more ammo";
+    }
+
+    public void Start (){
+        Health.RunPowerUp();
+        /*
+        After prompt the game for a name we:
+        Enter a cave
+        find Health.
+        Meet a dragon. (need an enemy class)
+        Pick weapon.
+        Battle dragon. (Battle class)
+        If dragon wins: get Health and ammo
+        if dragon wins: lose Health;
+        */
+    }
+
+    //Game PowerUps
+    public PowerUpBase Health = new PowerUpBase();
+    public PowerUpBase Ammo = new PowerUpBase();
+
+    //Game Weapons
+    private WeaponBase Gun = new WeaponBase();
+
+    private WeaponBase Rifle = new WeaponBase();
+
+    private WeaponBase Knife = new WeaponBase();
+
+    public string name;
+
+    private int score;
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,15 @@
-﻿    using System;
-    public class Program
+using System;
+
+public class Program
+{
+
+    public static Game myGame = new Game();
+
+    public static void Main(string[] args)
     {
-
-        public static PowerUp Health = new PowerUp();
-        
-        public static PowerUp Ammo = new PowerUp();
-        public static void Main(string[] args)
-        {
-            Health = new PowerUp();
-            Health.duration = 10;
-            Health.RunPowerUp();
-            Console.WriteLine(Health.duration);
-
-            Health.RechargePowerUp(20);
-            Ammo.RechargePowerUp(300);
-        }
+        Console.WriteLine("Please type in your name");
+        myGame.name = Console.ReadLine();
+        Console.WriteLine("Your Player Name is" + myGame.name);
+        myGame.Start();
     }
-
+}
